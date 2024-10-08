a6a72bc98fdef511f7523bc1bf74ea38044ec76c
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2024-10-08 15:16:29 +0530

    frontend: Shad CN + file upload functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,19 @@ import './App.css'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './home'
 import Form from './form'
+import TestPage from './pages/TestPage'
+import FileUploadForm from './pages/fire'
+import CreateBlink from './pages/CreateBlink'
+
 function App() {
   return (
-
     <Router>
       <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<Form />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/create" element={<CreateBlink />} />
+        <Route path="/fire" element={<FileUploadForm />} />
       </Routes>
     </Router>
   )
